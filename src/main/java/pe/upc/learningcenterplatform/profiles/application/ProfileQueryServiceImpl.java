1e7bc8cf58992452c030f245a8d89f177b1fc34c
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-15 21:25:48 -0500

    feat(profiles): add implements of Profile Query Services

---
+++ src/main/java/pe/upc/learningcenterplatform/profiles/application/ProfileQueryServiceImpl.java
@@ -0,0 +1,35 @@
+package pe.upc.learningcenterplatform.profiles.application;
+
+import pe.upc.learningcenterplatform.profiles.domain.model.aggregates.Profile;
+import pe.upc.learningcenterplatform.profiles.domain.model.queries.GetAllProfilesQuery;
+import pe.upc.learningcenterplatform.profiles.domain.model.queries.GetProfileByEmailQuery;
+import pe.upc.learningcenterplatform.profiles.domain.model.queries.GetProfileByIdQuery;
+import pe.upc.learningcenterplatform.profiles.domain.services.ProfileQueryService;
+import pe.upc.learningcenterplatform.profiles.infrastructure.persistence.jpa.repositories.ProfileRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProfileQueryServiceImpl implements ProfileQueryService {
+
+    private final ProfileRepository profileRepository;
+
+    public ProfileQueryServiceImpl(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    @Override
+    public Optional<Profile> handle(GetProfileByEmailQuery query) {
+        return this.profileRepository.findByEmail(query.emailAddress());
+    }
+
+    @Override
+    public Optional<Profile> handle(GetProfileByIdQuery query) {
+        return this.profileRepository.findById(query.id());
+    }
+
+    @Override
+    public List<Profile> handle(GetAllProfilesQuery query) {
+        return this.profileRepository.findAll();
+    }
+}
