c4a7421e83eec0b65b86c6bbbbe06edb77162154
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-22 21:40:00 -0500

    feat(courses): add course aggregate

--- src/main/java/pe/upc/learningcenterplatform/learning/domain/model/aggregates/Course.java
+++ src/main/java/pe/upc/learningcenterplatform/learning/domain/model/aggregates/Course.java
@@ -1,7 +1,11 @@
 package pe.upc.learningcenterplatform.learning.domain.model.aggregates;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import lombok.Getter;
+import org.apache.logging.log4j.util.Strings;
+import pe.upc.learningcenterplatform.learning.domain.model.valueobjects.LearningPath;
+import pe.upc.learningcenterplatform.learning.domain.model.valueobjects.TutorialId;
 import pe.upc.learningcenterplatform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
 @Getter
@@ -9,4 +13,29 @@ import pe.upc.learningcenterplatform.shared.domain.model.aggregates.AuditableAbs
 public class Course extends AuditableAbstractAggregateRoot<Course> {
     private String title;
     private String description;
+
+    @Embedded
+    private final LearningPath learningPath;
+
+    public Course() {
+        this.title = Strings.EMPTY;
+        this.description = Strings.EMPTY;
+        this.learningPath = new LearningPath();
+    }
+
+    public Course updateInformation(String title, String description) {
+        this.title = title;
+        this.description = description;
+        return this;
+    }
+
+    public void addTutorialToLearningPath(TutorialId tutorialId) {
+        this.learningPath.addItem(this, tutorialId);
+    }
+
+    public void addTutorialToLearningPath(TutorialId tutorialId, TutorialId nextTutorialId) {
+        this.learningPath.addItem(this, tutorialId, nextTutorialId);
+    }
+
+
 }
--- src/main/java/pe/upc/learningcenterplatform/learning/domain/model/valueobjects/LearningPath.java
+++ src/main/java/pe/upc/learningcenterplatform/learning/domain/model/valueobjects/LearningPath.java
@@ -1,4 +1,90 @@
 package pe.upc.learningcenterplatform.learning.domain.model.valueobjects;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.OneToMany;
+import pe.upc.learningcenterplatform.learning.domain.model.aggregates.Course;
+import pe.upc.learningcenterplatform.learning.domain.model.entities.LearningPathItem;
+
+import java.util.List;
+import java.util.Objects;
+
+@Embeddable
 public class LearningPath {
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+    private List<LearningPathItem> learningPathItems;
+
+    public LearningPath() {
+        this.learningPathItems = List.of();
+    }
+
+    /**
+     * Check if the learning path is empty
+     * @return True if the learning path is empty, false otherwise
+     */
+    public boolean isEmpty() {
+        return learningPathItems.isEmpty();
+    }
+
+    /**
+     * Get the last tutorial in the learning path
+     * @return The id of the last tutorial in the learning path
+     */
+    public LearningPathItem getLastItemInLearningPath() {
+        return learningPathItems.stream()
+                .filter(item -> Objects.isNull(item.getNextItem()))
+                .findFirst()
+                .orElse(null);
+    }
+
+
+    public LearningPathItem getLearningPathItemWithTutorialId(TutorialId tutorialId) {
+        return learningPathItems.stream()
+                .filter(item -> item.getTutorialId().equals(tutorialId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    /**
+     * Add a new item to the learning path
+     * @param course The course of the new item
+     * @param tutorialId The tutorial id of the new item
+     */
+    public void addItem(Course course, TutorialId tutorialId) {
+        // Add the new item to the end of learning path
+        LearningPathItem learningPathItem = new LearningPathItem(course, tutorialId, null);
+        LearningPathItem originalLastItem = null;
+
+        if (!isEmpty()) originalLastItem = getLastItemInLearningPath();
+
+        learningPathItems.add(learningPathItem);
+
+        if (!Objects.isNull(originalLastItem)) originalLastItem.updateNextItem(learningPathItem);
+
+    }
+
+    /**
+     * Add a new item to the learning path with next item
+     * @param course The course of the new item
+     * @param tutorialId The tutorial id of the new item
+     * @param nextItem The next item in the learning path
+     */
+    public void addItem(Course course, TutorialId tutorialId, LearningPathItem nextItem){
+        // Add the new item to the learning path before the next item
+        LearningPathItem learningPathItem = new LearningPathItem(course, tutorialId, nextItem);
+        learningPathItems.add(learningPathItem);
+    }
+
+    /**
+     * Add a new item to the learning path before the next item
+     * @param course The course of the new item
+     * @param tutorialId The tutorial id of the new item
+     * @param nextTutorialId The tutorial id of the next item
+     */
+    public void addItem(Course course, TutorialId tutorialId, TutorialId nextTutorialId) {
+        // Add the new item to the learning path before the next item
+        LearningPathItem nextLearningPathItem = getLearningPathItemWithTutorialId(nextTutorialId);
+        addItem(course, tutorialId, nextTutorialId);
+    }
 }
