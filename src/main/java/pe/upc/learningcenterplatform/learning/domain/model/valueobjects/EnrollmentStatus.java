6338aa451cdefceb5af671130091201ee3f913a6
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-22 20:18:32 -0500

    feat(enrollments): add enrollment valueobjects

--- src/main/java/pe/upc/learningcenterplatform/learning/domain/model/valueobjects/EnrollmentStatus.java
+++ src/main/java/pe/upc/learningcenterplatform/learning/domain/model/valueobjects/EnrollmentStatus.java
@@ -1,4 +1,15 @@
 package pe.upc.learningcenterplatform.learning.domain.model.valueobjects;
 
+/**
+ * Enumeration representing the status of an enrollment.
+ * @summary
+ * This enumeration is used to represent the status of an enrollment.
+ * The possible values are: REQUESTED, CONFIRMED, REJECTED, CANCELLED.
+ * @since 1.0
+ */
 public enum EnrollmentStatus {
+    REQUESTED,
+    CONFIRMED,
+    REJECTED,
+    CANCELLED
 }
--- src/main/java/pe/upc/learningcenterplatform/learning/domain/model/valueobjects/ProgressStatus.java
+++ src/main/java/pe/upc/learningcenterplatform/learning/domain/model/valueobjects/ProgressStatus.java
@@ -1,4 +1,14 @@
 package pe.upc.learningcenterplatform.learning.domain.model.valueobjects;
 
+/**
+ * Enumeration representing the progress status.
+ * @summary
+ * This enumeration is used to represent the progress status.
+ * The possible values are: NOT_STARTED, STARTED, COMPLETED.
+ * @since 1.0
+ */
 public enum ProgressStatus {
+    NOT_STARTED,
+    STARTED,
+    COMPLETED
 }
