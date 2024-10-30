138eca763aa37c89611c321bc36ed5c7203fc673
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-29 20:34:07 -0500

    docs: add documentation comments to update student metrics on tutorial completed command

--- src/main/java/pe/upc/learningcenterplatform/learning/domain/model/commands/UpdateStudentMetricsOnTutorialCompletedCommand.java
+++ src/main/java/pe/upc/learningcenterplatform/learning/domain/model/commands/UpdateStudentMetricsOnTutorialCompletedCommand.java
@@ -2,7 +2,18 @@ package pe.upc.learningcenterplatform.learning.domain.model.commands;
 
 import pe.upc.learningcenterplatform.learning.domain.model.valueobjects.AcmeStudentRecordId;
 
+/**
+ * Command to update sstudent metrics on tutorial completed
+ * @param acmeStudentRecordId the student record id
+ *                            Cannot be null or empty
+ */
 public record UpdateStudentMetricsOnTutorialCompletedCommand(AcmeStudentRecordId acmeStudentRecordId) {
+    /**
+     * Constructor
+     * @param acmeStudentRecordId the student record id
+     *                            Cannot be null or empty
+     * @throws IllegalArgumentException if studentRecordId is null or empty
+     */
     public UpdateStudentMetricsOnTutorialCompletedCommand{
         if(acmeStudentRecordId == null || acmeStudentRecordId.studentRecordId() == null || acmeStudentRecordId.studentRecordId().isBlank()){
             throw new IllegalArgumentException("studentRecordId cannot be null or empty");
