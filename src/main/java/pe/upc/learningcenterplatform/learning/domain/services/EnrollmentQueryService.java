44267c44c0ed7e8d9694abb0575b2505ba3a6ad9
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-31 19:50:11 -0500

    feat(enrollments): add command and query service contracts

--- src/main/java/pe/upc/learningcenterplatform/learning/domain/services/EnrollmentQueryService.java
+++ src/main/java/pe/upc/learningcenterplatform/learning/domain/services/EnrollmentQueryService.java
@@ -1,5 +1,20 @@
 package pe.upc.learningcenterplatform.learning.domain.services;
 
+import pe.upc.learningcenterplatform.learning.domain.model.aggregates.Enrollment;
+import pe.upc.learningcenterplatform.learning.domain.model.queries.*;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface EnrollmentQueryService {
 
+    Optional<Enrollment> handle(GetEnrollmentByIdQuery query);
+
+    List<Enrollment> handle(GetAllEnrollmentsQuery query);
+
+    List<Enrollment> handle(GetAllEnrollmentsByAcmeStudentRecordIdQuery query);
+
+    List<Enrollment> handle(GetAllEnrollmentsByCourseIdQuery query);
+
+    Optional<Enrollment> handle(GetEnrollmentByAcmeStudentRecordIdAndCourseIdQuery query);
 }
